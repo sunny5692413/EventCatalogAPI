fb401a0c07a07d9ad4632fcdf835fb2ce09ca14c
haeleemlee <48168366+haeleemlee@users.noreply.github.com>
2019-09-28 04:09:36 -0700

    Add EventController.cs and edit appsettings.json for filtering event with query
    At the postman, the query order should be entered the following manner
    http://localhost:63877/api/event/items/type/[eventTypeId]/location/[eventLocationId]/price/[eventPriceId]
    localhost: number is upon your localhost number under your property->debug.
    [eventTypeId], [eventLocationId],[eventPriceId] are numbers assigned on EventSeed.cs

---
+++ Controllers/EventController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventCatalogAPI.Data;
+using EventCatalogAPI.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EventCatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly EventContext _context;
+        private readonly IConfiguration _config;
+        public EventController(EventContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> Items(
+            [FromQuery] int pageIndex = 0,
+            [FromQuery] int pageSize = 6)
+        {
+            var itemCount = await _context.EventItems.LongCountAsync();
+            var items = await _context.EventItems
+                .OrderBy(c => c.Name)
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            items = ChangePictureUrl(items);
+
+            return Ok(items);               
+        }
+
+        [HttpGet]
+        [Route("[action]/type/{eventTypeId}/location/{eventLocationId}/price/{eventPriceId}")]
+        public async Task<IActionResult> Items(
+            int? eventTypeId,
+            int? eventLocationId,
+            int? eventPriceId,
+            [FromQuery] int pageIndex = 0,
+            [FromQuery] int pageSize = 6)
+        {
+            var root = (IQueryable<EventItem>)_context.EventItems;
+            if (eventTypeId.HasValue)
+            {
+                root = root.Where(c => c.EventTypeId == eventTypeId);
+            }
+            if (eventLocationId.HasValue)
+            {
+                root = root.Where(c => c.EventLocationId == eventLocationId);
+            }
+            if (eventPriceId.HasValue)
+            {
+                root = root.Where(c => c.EventPriceId == eventPriceId);
+            }
+
+            var itemsCount = await root.LongCountAsync();
+            var items = await root
+                .OrderBy(c => c.Name)
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            items = ChangePictureUrl(items);
+
+            return Ok(items);
+        }
+
+
+        private List<EventItem> ChangePictureUrl(List<EventItem> items)
+        {
+            items.ForEach(c => c.PictureUrl = c.PictureUrl.Replace("http://externalcatalogbaseurltobereplaced", _config["ExternalEventBaseUrl"]));
+            return items;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> EventTypes()
+        {
+            var items = await _context.EventTypes.ToListAsync();
+            return Ok(items);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> EventLocations()
+        {
+            var items = await _context.EventLocations.ToListAsync();
+            return Ok(items);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> EventPrices()
+        {
+            var items = await _context.EventPrices.ToListAsync();
+            return Ok(items);
+        }
+    }
+}
